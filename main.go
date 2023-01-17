0d97d84c96edb47d60b6fea47e7d957a45f03346
Thiago <thiagomowszet@gmail.com>
2023-01-16 22:02:06 -0300

    all functions and working...

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ type Movie struct {
     ID string `json:"id"`
     Isbn string  `json:"isbn"`
     Title string `json:"title"`
-    Director *Director `json:"director`
+    Director *Director `json:"director"`
 }
 
 
@@ -43,9 +43,53 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
     for index, item := range movies {
         if item.ID == params["id"]{
             movies = append(movies[:index], movies[index+1:]...)
+            break
+        }
+    }
+    json.NewEncoder(w).Encode(movies)
+}
+
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+    for _, item := range movies {
+        if item.ID == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
         }
     }
 }
+
+
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    var movie Movie
+    _ = json.NewDecoder(r.Body).Decode(&movie)
+    movie.ID = strconv.Itoa(rand.Intn(100000000))
+    movies = append(movies, movie)
+    json.NewEncoder(w).Encode(movie)
+}
+
+
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+    for index, item := range movies{
+        if item.ID == params["id"] {
+            movies = append(movies[:index], movies[index+1:]...)
+            var movie Movie
+            _ = json.NewDecoder(r.Body).Decode(&movie)
+            movie.ID = params["id"]
+            movies = append(movies, movie)
+            json.NewEncoder(w).Encode(movie)
+        }
+        
+    }
+}
+
 
 
 
@@ -60,4 +104,8 @@ func main() {
     r.HandleFunc("movies", createMovie).Methods("POST")
     r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
     r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+
+    fmt.Printf("Starting server at port 8080\n")
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
